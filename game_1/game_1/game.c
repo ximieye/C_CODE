823e1713c637fc0fd03904a111179ec6b5b111dd
ximieye <1565865244@qq.com>
2023-07-15 16:11:54 +0800

    C语言学习过程代码（初阶）- 三子棋游戏

---
+++ game_1/game_1/game.c
@@ -0,0 +1,180 @@
+#include "game.h"
+
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+//版本一：
+//void DisplayBoard(char board[ROW][COL], int row, int col)
+//{
+//	int i = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		//打印数据
+//		printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+//		//打印分割行
+//		if (i < row - 1)
+//			printf("---|---|---\n");
+//	}
+//}
+
+//版本二：
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		//打印数据
+		//printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j < col - 1)
+				printf("|");
+		}
+		printf("\n");
+		//打印分割行		
+		//printf("---|---|---\n");
+		if (i < row - 1)
+		{
+			int j = 0;
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j < col - 1)
+					printf("|");
+			}
+			printf("\n");
+		}
+	}
+}
+
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+
+	printf("玩家下棋:>\n");
+
+	while (1)
+	{
+		printf("请输入坐标:>");
+		scanf("%d %d", &x, &y);
+		//坐标范围合法的判断
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("坐标被占用，不能下棋,请选择其他位置\n");
+			}
+		}
+		else
+		{
+			printf("坐标非法，请重新输入\n");
+		}
+	}
+}
+
+void ComputerMove(char board[ROW][COL], int row, int col)
+{
+	printf("电脑下棋:>\n");
+
+	int x = 0;
+	int y = 0;
+
+	while (1)
+	{
+		x = rand() % row;//0~2
+		y = rand() % col;//0~2
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '#';
+			break;
+		}
+	}
+}
+
+//满了就返回1
+//不满就返回0
+int IsFull(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;
+			}
+		}
+	}
+
+	return 1;
+}
+
+char IsWin(char board[ROW][COL], int row, int col)
+{
+	//一方赢了
+	//行赢
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')
+		{
+			return board[i][1];
+		}
+
+	}
+	//列赢
+	int j = 0;
+	for (j = 0; j < col; j++)
+	{
+		if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[1][j] != ' ')
+		{
+			return board[1][j];
+		}
+	}
+	//对角线赢
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+
+	//没有人赢，就是平局
+	if (IsFull(board, row, col))
+	{
+		return 'Q';
+	}
+
+	//游戏继续
+	return 'C';
+}
+
+
+
+
+
+
+
+
---
+++ game_1/game_1/game.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ROW 3
+#define COL 3
+
+//初始化棋盘
+void InitBoard(char board[ROW][COL], int row, int col);
+
+//打印棋盘
+void DisplayBoard(char board[ROW][COL], int row, int col);
+
+//玩家下棋
+void PlayerMove(char board[ROW][COL],int row,int col);
+
+//电脑下棋
+//找没有下棋的位置随机下
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+//判断输赢
+//玩家赢 - '*'
+//电脑赢 - '#'
+//平局 - 'Q'
+//继续 - 'C'
+char IsWin(char board[ROW][COL], int row, int col);
